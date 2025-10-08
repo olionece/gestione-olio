d32a9bbf6bf78a05b66a5c8894208cc42cbb22f4
olionece <patrick.zinelli@gmail.com>
2025-10-08 15:09:33 +0200

    page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -340,9 +340,7 @@ function MovementForm({ onInserted, warehouses, wh }: {
     })();
   }, []);
 
-  useEffect(() => {const { data, error } = await supabase.from('warehouses').select('id,name').order('name');
-console.log('WAREHOUSES DATA:', data, 'ERROR:', error);
-
+  useEffect(() => {
     if (wh !== 'all') setWarehouseId(wh);
     else if (warehouses[0]) setWarehouseId(warehouses[0].id);
   }, [wh, warehouses]);
